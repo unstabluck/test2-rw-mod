8838db3eb8d0e1ec4b09e2f3177a8968ed39e092
Manngo <unstabluck@gmail.com>
2025-09-20 11:22:05 -0400

    It builds now

--- src/ControlSystem.cs
+++ src/ControlSystem.cs
@@ -14,39 +14,70 @@ namespace TestMod;
 [BepInPlugin("manngo.spider_revive_explosion", "Spiders Explode on Revive", "0.1.0")]
 internal class FreeHomingMath
 {
-    public AbstractPhysicalObject target;
-    public Room room = target.Room;
+    public BodyChunk target;
+    public Room room;
     public Vector2 lastPos;
     public Vector2 pos;
     public Vector2 lastVel;
     public Vector2 vel;
     public Vector2 lastAccel;
-    public Vector2 Accel;
-    public Vector2 targetlastPos;
+    public Vector2 accel;
+    public Vector2 targetLastPos;
     public Vector2 targetPos;
-    public Vector2 targetlastVel;
+    public Vector2 targetLastVel;
     public Vector2 targetVel;
-    public Vector2 targetlastAccel;
+    public Vector2 targetLastAccel;
     public Vector2 targetAccel;
+    public Vector2 errorp;
+    public Vector2 errori;
+    public Vector2 errord;
+    public double kp = 0.05;
+    public double ki = 0.05;
+    public double kd = 0.05;
 
-    public FreeHomingMath(AbstractPhysicalObject target, Vector2 pos) { 
+    public FreeHomingMath(BodyChunk target, Vector2 pos, Room room) { 
         this.target = target;
         this.pos = pos;
-        this.room = target.Room;
+        this.room = room;
         this.targetPos = target.pos;
+
+        this.lastPos = pos;
+        this.lastVel = Vector2.zeroVector;
+        this.lastAccel = Vector2.zeroVector;
+        targetLastVel = Vector2.zeroVector;
+        targetLastAccel = Vector2.zeroVector;
+
+        targetVel = Vector2.zeroVector;
+        targetAccel = Vector2.zeroVector;
+
+
 
     }
 
     public void Update()
     {
+        errorp = new Vector2(vel.x - targetVel.x, vel.y - targetVel.y);
+        errori = new Vector2(pos.x - targetPos.x, pos.y - targetPos.y);
+        errord = new Vector2(accel.x - targetAccel.x, accel.y - targetAccel.y);
+
+        vel = new Vector2(pos.x -lastPos.x, pos.y - lastPos.y);
+        targetVel = new Vector2(targetPos.x - targetLastPos.x, targetPos.y - targetLastPos.y);
+        accel = new Vector2(vel.x - lastVel.x, vel.y - lastVel.y);
+        targetAccel = new Vector2(targetVel.x - targetLastVel.x, targetVel.y - targetLastVel.y);
+        
+
 
         lastPos = pos;
         lastVel = vel;
-        lastAccel = Accel;
+        lastAccel = accel;
         targetLastPos = targetPos;
-        targetlastVel = targetVel
+        targetLastVel = targetVel;
         targetLastAccel = targetAccel;
         //TODO add math and explosions 
+
+        pos = pos + (errori * ki);
+        vel = vel + (errorp * kp);
+        accel = accel + (errord * kd);
     }
 
     
--- src/Plugin.cs
+++ src/Plugin.cs
@@ -18,7 +18,7 @@ sealed class Plugin : BaseUnityPlugin
 {
     public static new ManualLogSource Logger;
     bool IsInit;
-    List<FreeHomingMath> boomFlies = new List<FreeHomingMath>;
+    public List<FreeHomingMath> boomFlies = new List<FreeHomingMath>();
 
     public void OnEnable()
     {
@@ -38,10 +38,10 @@ sealed class Plugin : BaseUnityPlugin
         // Initialize assets, your mod config, and anything that uses RainWorld here
         Logger.LogDebug("Hello world!");
     }
-    public static void MyReviveFunc(On.BigSpider.orig_Revive orig, BigSpider self)
+    public void MyReviveFunc(On.BigSpider.orig_Revive orig, BigSpider self)
     {
         orig(self);
-        self.room.PlayersInRoom[0]
+        Player player = self.room.PlayersInRoom[0];
         var pos = self.mainBodyChunk.pos;
         
         Logger.LogInfo("Boom");
@@ -51,16 +51,31 @@ sealed class Plugin : BaseUnityPlugin
         self.room.AddObject(new ExplosionSpikes(self.room, pos, 14, 30f, 9f, 7f, 170f, new UnityEngine.Color(1f, 1f, 1f)));
         self.room.AddObject(new ShockWave(pos, 330f, 0.045f, 5, false));
         self.room.PlaySound(SoundID.Bomb_Explode, self.mainBodyChunk.pos);
-        boomFlies.Add( new FreeHomingMath(self.room.PlayersInRoom[0], pos) );
+        this.boomFlies.Add(new FreeHomingMath(self.room.PlayersInRoom[0].mainBodyChunk, pos, self.room) );
         
     }
 
-    public static void MyUpdateFunc(On.RainWorldGame.orig_Update orig, RainWorldGame self)
+    public void MyUpdateFunc(On.RainWorldGame.orig_Update orig, RainWorldGame self)
     {
         orig(self);
-        for (int i = 0;i < boomFlies.LastIndexOf; i++) {
+        for (int i = 0; i < this.boomFlies.Count; i++)
+        {
+            Logger.LogInfo("inside update-for boomflies loop");
             boomFlies[i].Update();
-            
+            Vector2 boomPos = boomFlies[i].pos;
+            Room room = boomFlies[i].room;
+            Player owner = boomFlies[i].room.PlayersInRoom[0];
+
+            Logger.LogInfo("("boomPos.x+", "+boomPos.y+")");
+            self.room.AddObject(new Explosion(room, owner, boomPos, 7, 250f, 6.2f, 2f, 280f, 0.25f, owner, 0.7f, 160f, 1f));
+            self.room.AddObject(new Explosion.ExplosionLight(boomPos, 280f, 1f, 7, new UnityEngine.Color(1f, 1f, 1f)));
+            self.room.AddObject(new Explosion.ExplosionLight(boomPos, 230f, 1f, 3, new UnityEngine.Color(1f, 1f, 1f)));
+            self.room.AddObject(new ExplosionSpikes(room, boomPos, 14, 30f, 9f, 7f, 170f, new UnityEngine.Color(1f, 1f, 1f)));
+            self.room.AddObject(new ShockWave(boomPos, 330f, 0.045f, 5, false));
+            self.room.PlaySound(SoundID.Bomb_Explode, boomPos);
+
+
+        }
     }
     
 }
